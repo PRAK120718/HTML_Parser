d447425685a55c18185b1b62fb6f3c89237638ad
Prakarsh Anand <prakarsh.a@bounceshare.com>
2020-08-02 15:10:00 +0530

    0.1

--- src/main/java/com/htmlparser/driver/HTML_Parser.java
+++ src/main/java/com/htmlparser/driver/HTML_Parser.java
@@ -1,14 +1,19 @@
 package com.htmlparser.driver;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigList;
+import com.typesafe.config.ConfigValue;
 import org.jsoup.Jsoup;
-import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import us.codecraft.xsoup.Xsoup;
-
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,113 +21,71 @@ public class HTML_Parser {
 
     public static void main(String args[]) throws IOException {
 
-
-        JSON_Obj hp = new JSON_Obj();
+        Config conf = ConfigFactory.load();
         File input = new File("/Users/prakarsh/Desktop/HTML_Parser/src/main/resources/HTML.html");
         Document doc= Jsoup.parse(input,"UTF-8");
-         Element table = doc.body();
+        Element body = doc.body();
+        HashMap<String,String> json_obj = new HashMap<String, String>();
 
 
-         // fetching the property address
+        Config required_fields = conf.getConfig("required");
 
-        Elements address = table.getElementsByAttributeValueMatching("style", "italic");
-        for (Element x : address) {
-            String pattern = x.html();
-            Pattern p = Pattern.compile("[0-9]{3,}.*");
-            Matcher matcher = p.matcher(pattern);
-            if (matcher.find()) {
-                pattern = matcher.group();
-                pattern = pattern.replace("\"", "");
-            }
-            Elements realtor = table.getElementsByAttributeValueMatching("href", "http://email.realtor.com.*");
-            for (Element y : realtor) {
-                String value = y.html();
-                if (value.toLowerCase().contains(pattern.toLowerCase())) {
-                    hp.property_address = value;
+
+        for( Map.Entry<String, ConfigValue> required_field : required_fields.entrySet()){
+
+            Config required_field_xpaths = conf.getConfig(required_field.getKey()+".xpath");
+            ConfigList required_field_keys = required_fields.getList(required_field.getKey());
+
+            for( ConfigValue required_field_key : required_field_keys){
+
+                String key = required_field_key.unwrapped().toString();
+                json_obj.put(key,null);
+                if(required_field_xpaths.hasPath(key)){
+
+                    String required_field_xpath = required_field_xpaths.getValue(key).unwrapped().toString();
+                    Elements required_field_element = Xsoup.compile(required_field_xpath).evaluate(doc).getElements();
+                    String required_field_value = required_field_element.html();
+//                    System.out.println(required_field_value);
+                    json_obj.put(key,required_field_value);
+                }
+                else
+                {
+                    if(key.equals("address")){
+//                         fetching the property address
+                    Elements address = body.getElementsByAttributeValueMatching("style", "italic");
+                    for (Element x : address) {
+                        String pattern = x.html();
+                        Pattern p = Pattern.compile("[0-9]{3,}.*");
+                        Matcher matcher = p.matcher(pattern);
+                        if (matcher.find()) {
+                            pattern = matcher.group();
+                            pattern = pattern.replace("\"", "");
+                            }
+                        Elements realtor = body.getElementsByAttributeValueMatching("href", "http://email.realtor.com.*");
+                        for (Element y : realtor)
+                        {
+                        String value = y.html();
+                        if (value.toLowerCase().contains(pattern.toLowerCase())) {
+                            json_obj.put("address",value);
+                                }
+                            }
+                        }
+
+                    }
                 }
             }
         }
 
 
-
-        //fetching buyer email,contact
-
-        //xsoup to use XPath
-
-        Elements contact = Xsoup.compile("/html/body/table/tbody/tr[3]/td/table/tbody/tr[2]/td[2]/table/tbody/tr[3]/td/font/a").evaluate(doc).getElements();
-        hp.buyer_contact = contact.html();
-        Elements email = Xsoup.compile("/html/body/table/tbody/tr[3]/td/table/tbody/tr[2]/td[2]/table/tbody/tr[4]/td/font/a").evaluate(doc).getElements();
-        hp.buyer_email = email.html();
-        Elements name = Xsoup.compile("/html/body/table/tbody/tr[3]/td/table/tbody/tr[1]/td[1]/table/tbody/tr[8]/td[2]/font/strong").evaluate(doc).getElements();
-        hp.buyer_name = name.html();
-        Elements beds = Xsoup.compile("/html/body/table/tbody/tr[8]/td[2]/table/tbody/tr/td/table/tbody/tr[2]/td/font[3]/strong[1]").evaluate(doc).getElements();
-        hp.property_beds =beds.html();
-        Elements baths = Xsoup.compile("/html/body/table/tbody/tr[8]/td[2]/table/tbody/tr/td/table/tbody/tr[2]/td/font[3]/strong[2]").evaluate(doc).getElements();
-        hp.property_baths = baths.html();
-//        Elements body_buyer=table.getElementsByAttributeValueMatching("style","border: 1px solid #ddd.*");
-//        for( Element body: body_buyer) {
-//
-//
-//            Elements buyer_contact = body.getElementsByAttributeValueMatching("href", "tel.*");
-//
-//            for (Element x : buyer_contact) {
-//
-////                    System.out.println(x.html());
-//                    hp.buyer_contact=x.html();
-//
-//            }
-//            Elements buyer_mail = body.getElementsByAttributeValueMatching("href","mailto.*");
-//            for (Element x : buyer_mail) {
-////                System.out.println(x.html());
-//                hp.buyer_email = x.html();
-//            }
-//
-//
-//            break;
-//        }
-//
-//        //fetching buyer name
-//        Elements body_property=table.getElementsByClass("font16");
-//        for( Element body: body_property) {
-//
-//            if(body.html().contains("Name")){
-//                Elements vals = body.getElementsByTag("Strong");
-//                for(Element name_val : vals)
-//                {
-////                    System.out.println(name_val.html());
-//                    hp.buyer_name=name_val.html();
-//                }
-//            }
-//        }
-//
-//        //fetching property details
-//        Elements property_details = table.getElementsByClass("font12");
-//        for( Element body: property_details) {
-//
-//
-//            if(body.html().contains("Beds ")){
-//
-//                Elements vals = body.getElementsByTag("Strong");
-//                for(Element name_val : vals)
-//                {
-//                    hp.property[i++]=name_val.html();
-////                    System.out.println(hp.property[i-1]);
-//                }
-//            }
-//
-//        }
-//        for(String x : hp.property)
-//        {
-//            System.out.println(x);
-//        }
-//        System.out.println(hp.buyer_contact);
-//        System.out.println(hp.buyer_email);
-//        System.out.println(hp.buyer_name);
 //
         //Creating the ObjectMapper object
         ObjectMapper mapper = new ObjectMapper();
-        String jsonString = mapper.writeValueAsString(hp);
+        String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json_obj);
         System.out.println(jsonString);
+        FileWriter file = new FileWriter("/Users/prakarsh/Desktop/HTML_Parser/src/main/resources/output.json");
+        file.write(jsonString);
+        file.close();
+
 
     }
 
