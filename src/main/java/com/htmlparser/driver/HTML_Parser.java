882e72b74a9f6549af1056228d962e1a023aab0e
Prakarsh Anand <prakarsh.a@bounceshare.com>
2020-08-02 11:32:51 +0530

    the Xsoup way

--- src/main/java/com/htmlparser/driver/HTML_Parser.java
+++ src/main/java/com/htmlparser/driver/HTML_Parser.java
@@ -21,9 +21,7 @@ public class HTML_Parser {
         File input = new File("/Users/prakarsh/Desktop/HTML_Parser/src/main/resources/HTML.html");
         Document doc= Jsoup.parse(input,"UTF-8");
          Element table = doc.body();
-         int no_of_propeties =3;
-         hp.property = new String[no_of_propeties];
-         int i=0;
+
 
          // fetching the property address
 
@@ -40,7 +38,7 @@ public class HTML_Parser {
             for (Element y : realtor) {
                 String value = y.html();
                 if (value.toLowerCase().contains(pattern.toLowerCase())) {
-                    hp.property[i++] = value;
+                    hp.property_address = value;
                 }
             }
         }
@@ -51,71 +49,76 @@ public class HTML_Parser {
 
         //xsoup to use XPath
 
-//        System.out.println(Xsoup.compile("/html/body/table/tbody/tr[3]/td/table/tbody/tr[2]/td[2]/table/tbody/tr[3]/td/font/a").evaluate(doc).getElements());
-//        System.out.println(Xsoup.compile("/html/body/table/tbody/tr[3]/td/table/tbody/tr[2]/td[2]/table/tbody/tr[4]/td/font/a").evaluate(doc).getElements());
-//        System.out.println(Xsoup.compile("/html/body/table/tbody/tr[3]/td/table/tbody/tr[1]/td[1]/table/tbody/tr[8]/td[2]/font/strong").evaluate(doc).getElements());
-//        System.out.println(Xsoup.compile("//html/body/table/tbody/tr[8]/td[2]/table/tbody/tr/td/table/tbody/tr[2]/td/font[3]/strong[1]").evaluate(doc).getElements());
-//        System.out.println(Xsoup.compile("/html/body/table/tbody/tr[8]/td[2]/table/tbody/tr/td/table/tbody/tr[2]/td/font[3]/strong[2]").evaluate(doc).getElements());
-        Elements body_buyer=table.getElementsByAttributeValueMatching("style","border: 1px solid #ddd.*");
-        for( Element body: body_buyer) {
-
-
-            Elements buyer_contact = body.getElementsByAttributeValueMatching("href", "tel.*");
-
-            for (Element x : buyer_contact) {
-
-//                    System.out.println(x.html());
-                    hp.buyer_contact=x.html();
-
-            }
-            Elements buyer_mail = body.getElementsByAttributeValueMatching("href","mailto.*");
-            for (Element x : buyer_mail) {
-//                System.out.println(x.html());
-                hp.buyer_email = x.html();
-            }
-
-
-            break;
-        }
-
-        //fetching buyer name
-        Elements body_property=table.getElementsByClass("font16");
-        for( Element body: body_property) {
-
-            if(body.html().contains("Name")){
-                Elements vals = body.getElementsByTag("Strong");
-                for(Element name_val : vals)
-                {
-//                    System.out.println(name_val.html());
-                    hp.buyer_name=name_val.html();
-                }
-            }
-        }
-
-        //fetching property details
-        Elements property_details = table.getElementsByClass("font12");
-        for( Element body: property_details) {
-
-
-            if(body.html().contains("Beds ")){
-
-                Elements vals = body.getElementsByTag("Strong");
-                for(Element name_val : vals)
-                {
-                    hp.property[i++]=name_val.html();
-//                    System.out.println(hp.property[i-1]);
-                }
-            }
-
-        }
-        for(String x : hp.property)
-        {
-            System.out.println(x);
-        }
-        System.out.println(hp.buyer_contact);
-        System.out.println(hp.buyer_email);
-        System.out.println(hp.buyer_name);
-
+        Elements contact = Xsoup.compile("/html/body/table/tbody/tr[3]/td/table/tbody/tr[2]/td[2]/table/tbody/tr[3]/td/font/a").evaluate(doc).getElements();
+        hp.buyer_contact = contact.html();
+        Elements email = Xsoup.compile("/html/body/table/tbody/tr[3]/td/table/tbody/tr[2]/td[2]/table/tbody/tr[4]/td/font/a").evaluate(doc).getElements();
+        hp.buyer_email = email.html();
+        Elements name = Xsoup.compile("/html/body/table/tbody/tr[3]/td/table/tbody/tr[1]/td[1]/table/tbody/tr[8]/td[2]/font/strong").evaluate(doc).getElements();
+        hp.buyer_name = name.html();
+        Elements beds = Xsoup.compile("/html/body/table/tbody/tr[8]/td[2]/table/tbody/tr/td/table/tbody/tr[2]/td/font[3]/strong[1]").evaluate(doc).getElements();
+        hp.property_beds =beds.html();
+        Elements baths = Xsoup.compile("/html/body/table/tbody/tr[8]/td[2]/table/tbody/tr/td/table/tbody/tr[2]/td/font[3]/strong[2]").evaluate(doc).getElements();
+        hp.property_baths = baths.html();
+//        Elements body_buyer=table.getElementsByAttributeValueMatching("style","border: 1px solid #ddd.*");
+//        for( Element body: body_buyer) {
+//
+//
+//            Elements buyer_contact = body.getElementsByAttributeValueMatching("href", "tel.*");
+//
+//            for (Element x : buyer_contact) {
+//
+////                    System.out.println(x.html());
+//                    hp.buyer_contact=x.html();
+//
+//            }
+//            Elements buyer_mail = body.getElementsByAttributeValueMatching("href","mailto.*");
+//            for (Element x : buyer_mail) {
+////                System.out.println(x.html());
+//                hp.buyer_email = x.html();
+//            }
+//
+//
+//            break;
+//        }
+//
+//        //fetching buyer name
+//        Elements body_property=table.getElementsByClass("font16");
+//        for( Element body: body_property) {
+//
+//            if(body.html().contains("Name")){
+//                Elements vals = body.getElementsByTag("Strong");
+//                for(Element name_val : vals)
+//                {
+////                    System.out.println(name_val.html());
+//                    hp.buyer_name=name_val.html();
+//                }
+//            }
+//        }
+//
+//        //fetching property details
+//        Elements property_details = table.getElementsByClass("font12");
+//        for( Element body: property_details) {
+//
+//
+//            if(body.html().contains("Beds ")){
+//
+//                Elements vals = body.getElementsByTag("Strong");
+//                for(Element name_val : vals)
+//                {
+//                    hp.property[i++]=name_val.html();
+////                    System.out.println(hp.property[i-1]);
+//                }
+//            }
+//
+//        }
+//        for(String x : hp.property)
+//        {
+//            System.out.println(x);
+//        }
+//        System.out.println(hp.buyer_contact);
+//        System.out.println(hp.buyer_email);
+//        System.out.println(hp.buyer_name);
+//
         //Creating the ObjectMapper object
         ObjectMapper mapper = new ObjectMapper();
         String jsonString = mapper.writeValueAsString(hp);
--- src/main/java/com/htmlparser/driver/JSON_Obj.java
+++ src/main/java/com/htmlparser/driver/JSON_Obj.java
@@ -4,6 +4,7 @@ public class JSON_Obj {
     public String buyer_name;
     public String buyer_email;
   public String buyer_contact;
-   public String[] property;
-
+   public String property_address;
+    public String property_baths;
+    public String property_beds;
 }
